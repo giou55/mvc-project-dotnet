12cfe8fd5ca04a56b3f1ce2cb06d799985fc5bf0
giou55 <giourmetakis00@gmail.com>
2022-11-18 12:35:24 +0200

    changes

---
+++ Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc_project_dotnet.Models;
+
+namespace mvc_project_dotnet.Controllers
+{
+    public class BookController : Controller
+    {
+        private IBookRepository repository;
+
+        public IQueryable<Book> Books;
+
+        public BookController(IBookRepository bookRepo)
+        {
+            repository = bookRepo;
+        }
+        public ViewResult Index()
+        {
+            Books = repository.Books;
+            return View(Books);
+        }
+    }
+}
