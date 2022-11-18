eecece5d9b782e6fa91d314b70453d2592b9f6e1
giou55 <giourmetakis00@gmail.com>
2022-11-18 23:32:54 +0200

    changes

---
+++ Models/ViewModelFactory.cs
@@ -0,0 +1,19 @@
+﻿using mvc_project_dotnet.Models.ViewModels;
+
+namespace mvc_project_dotnet.Models
+{
+    public class ViewModelFactory
+    {
+        public static BookViewModel Details(Book p)
+        {
+            return new BookViewModel
+            {
+                Book = p,
+                Action = "Details",
+                ReadOnly = true,
+                Theme = "info",
+                ShowAction = false
+            };
+        }
+    }
+}
---
+++ Models/ViewModels/BookViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace mvc_project_dotnet.Models.ViewModels
+{
+    //This class will allow the controller to pass data and display settings to its view.
+    public class BookViewModel
+    {
+        //The Book property provides the data to display
+        public Book Book { get; set; } = new Book();
+
+        //The other properties configure aspects of how the content is presented to the user
+        public string Action { get; set; } = "Add";
+        public bool ReadOnly { get; set; } = false;
+        public string Theme { get; set; } = "primary";
+        public bool ShowAction { get; set; } = true;
+    }
+}
